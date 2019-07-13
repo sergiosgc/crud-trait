b077bc2f47afd7e241da34b039ccc4a8fb456acd
Sérgio Carvalho <sergio.carvalho@sooma.com>
2019-07-13 17:39:09 +0100

    Test that class is Describable on JsonSerializable and ArrayAccess

--- sergiosgc/crud/ArrayAccess.php
+++ sergiosgc/crud/ArrayAccess.php
@@ -3,6 +3,7 @@ namespace sergiosgc\crud;
 
 trait ArrayAccess {
     public function offsetExists ( $offset ) {
+        if (!$this instanceof Describable) throw new Exception("ArrayAccess trait can only be used by Describable classes");
         return in_array($offset, array_keys(static::describeFields()));
     }
     public function offsetGet ( $offset ) {
--- sergiosgc/crud/JsonSerializable.php
+++ sergiosgc/crud/JsonSerializable.php
@@ -3,6 +3,7 @@ namespace sergiosgc\crud;
 
 trait JsonSerializable {
     public function jsonSerialize ( ) {
+        if (!$this instanceof Describable) throw new Exception("JsonSerializable trait can only be used by Describable classes");
         $result = [];
         foreach (array_keys(static::describeFields()) as $k) $result[$k] = $this->$k;
         return $result;
