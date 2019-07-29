885e2d5114dd7f0bc7efd2a9faeda8c135c162f5
Sérgio Carvalho <sergio.carvalho@sooma.com>
2019-07-29 17:52:29 +0100

    Change signature of validation functions to include class

--- sergiosgc/crud/BaseValidations.php
+++ sergiosgc/crud/BaseValidations.php
@@ -2,19 +2,19 @@
 namespace sergiosgc\Crud;
 
 class BaseValidations {
-    public static function required($field, $values, $describedFields, $message = null) {
+    public static function required($field, $describedFields, $values, $class, $message = null) {
         $message = $message ?: _('Field is required');
         if (!isset($values[$field])) return [ $message ];
         return [];
     }
-    public static function nonempty($field, $values, $describedFields, $message = null) {
-        $result = static::required($field, $values, $describedFields, $message);
+    public static function nonempty($field, $describedFields, $values, $class, $message = null) {
+        $result = static::required($field, $describedFields, $values, $class, $message);
         if (count($result)) return $result;
         $message = $message ?: _('Field must be non-empty');
         if (is_string($values[$field]) && $values[$field] == '' || is_null($values[$field])) return [ $message ];
         return [];
     }
-    public static function regexp($field, $values, $describedFields, $regex, $message = null) {
+    public static function regexp($field, $describedFields, $values, $class, $regex, $message = null) {
         if (!isset($values[$field])) return [ ];
         $message = $message ?: sprintf(_('Field must match regex %s'), $regex);
         $result = preg_match($regex, $values[$field]);
--- sergiosgc/crud/Validator.php
+++ sergiosgc/crud/Validator.php
@@ -3,29 +3,36 @@ namespace sergiosgc\crud;
 
 class Validator {
     public static $validationFunctionMap = [
-        'required' => [ '\sergiosgc\crud\BaseValidations', 'required' ],
-        'nonempty' => [ '\sergiosgc\crud\BaseValidations', 'nonempty' ],
-        'regexp' => [ '\sergiosgc\crud\BaseValidations', 'regexp' ]
+        'required' => [
+            'callable' => [ '\sergiosgc\crud\BaseValidations', 'required' ]
+        ],
+        'nonempty' => [
+            'callable' => [ '\sergiosgc\crud\BaseValidations', 'nonempty' ],
+        ],
+        'regexp' => [ 
+            'callable' => [ '\sergiosgc\crud\BaseValidations', 'regexp' ]
+        ],
     ];
-    public static function registerValidationFunction($validation, $callable) {
-        if (!is_callable($callable)) throw new Exception('$callable must be a PHP is_callable()');
-        static::$validationFunctionMap[$callable] = $callable;
+    public static function registerValidationFunction($validation, $function) {
+        if (is_callable($function)) return static::registerValidationFunction($validation, [ 'callable' => $function ]);
+        if (!is_array($function) || !\array_key_exists('callable', $function)) throw new Exception('Invalid argument: $function');
+        if (!is_callable($function['callable'])) throw new Exception('function callable is not is_callable()');
+        static::$validationFunctionMap[$validation] = $function;
     }
-    public static function validateValues($describedFields, $values) {
+    public static function validateValues($describedFields, $values, $class = null) {
         $errors = [];
         foreach ($describedFields as $field => $description) {
             if (!isset($description['validation'])) continue;
             $fieldErrors = [];
             foreach ($description['validation'] as $validation) {
+                if (!isset(static::$validationFunctionMap[is_array($validation) ? $validation[0] : $validation])) continue;
+                $args = [$field, $describedFields, $values, $class];
+                if (\array_key_exists('args', static::$validationFunctionMap[$validation])) $args = array_merge($args, static::$validationFunctionMap[$validation]['args']);
                 if (is_array($validation)) {
-                    $args = array_merge([$field, $values, $describedFields], array_slice($validation, 1));
+                    $args = array_merge($args, array_slice($validation, 1));
                     $validation = $validation[0];
-                } else {
-                    $args = [$field, $values, $describedFields];
                 }
-                if (!isset(static::$validationFunctionMap[$validation])) continue;
-                if (!is_callable(static::$validationFunctionMap[$validation])) throw new Exception(sprintf('Validation for %s is not callable', $validation));
-                $fieldErrors = array_merge($fieldErrors, call_user_func_array(static::$validationFunctionMap[$validation], $args));
+                $fieldErrors = array_merge($fieldErrors, call_user_func_array(static::$validationFunctionMap[$validation]['callable'], $args));
             }
             if (count($fieldErrors)) $errors[$field] = $fieldErrors;
         }
