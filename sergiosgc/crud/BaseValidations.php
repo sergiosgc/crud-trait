4efa88d4109909eab9b63298f8f24553b5166888
Sérgio Carvalho <sergio.carvalho@sooma.com>
2023-08-25 12:21:57 +0100

    Add email base validation

--- sergiosgc/crud/BaseValidations.php
+++ sergiosgc/crud/BaseValidations.php
@@ -47,4 +47,11 @@ class BaseValidations {
         $duplicates = call_user_func_array( [ $class, 'dbReadPaged' ], $dbReadPagedArgs)[0];
         return count($duplicates) ? [ $message ] : [];
     }
+    public static function email($field, $describedFields, $values, $class, $message = null) {
+        if ( 0 == (int) preg_match('/^(?P<username>(?:[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"))@(?P<domain>(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))$/',
+                                   $values[$field])) {
+            return [ $message ?: __("Email must be in the format username@domain") ];
+        }
+        return [];
+    }
 }
