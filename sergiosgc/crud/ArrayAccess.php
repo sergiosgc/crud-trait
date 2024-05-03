f30017cdbc56057fdb36f3ecb2cf46a1c27b2a55
Sérgio Carvalho <sergio.carvalho@sooma.com>
2024-05-03 16:32:34 +0100

    Declare types in ArrayAccess trait

--- sergiosgc/crud/ArrayAccess.php
+++ sergiosgc/crud/ArrayAccess.php
@@ -2,20 +2,20 @@
 namespace sergiosgc\crud;
 
 trait ArrayAccess {
-    public function offsetExists ( $offset ) {
+    public function offsetExists ( mixed $offset ): bool {
         if (!$this instanceof Describable) throw new Exception("ArrayAccess trait can only be used by Describable classes");
         return in_array($offset, array_keys(static::describeFields()));
     }
-    public function offsetGet ( $offset ) {
+    public function offsetGet ( mixed $offset ): mixed {
         if (!$this->offsetExists($offset)) return null;
         return $this->$offset;
     }
-    public function offsetSet ( $offset , $value ) {
-        if (!$this->offsetExists($offset)) return null;
+    public function offsetSet ( mixed $offset , mixed $value ): void {
+        if (!$this->offsetExists($offset)) return;
         $this->$offset = $value;
     }
-    public function offsetUnset ( $offset ) {
-        if (!$this->offsetExists($offset)) return null;
+    public function offsetUnset ( mixed $offset ): void {
+        if (!$this->offsetExists($offset)) return;
         unset($this->$offset);
     }
 }
